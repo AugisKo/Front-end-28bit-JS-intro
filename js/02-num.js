b29eb5f6772fd3358b31f9781c4cbce29ef34a85
Eugenijus <augisk@gmail.com>
2021-05-04 20:08:17 +0300

    Numbers

---
+++ js/02-num.js
@@ -0,0 +1,29 @@
+/*
+NUMBERS
+teigiami, nulis, neigiami
+desimtainiai, sveiki
+tikri skaiciai, begalybe, NaN
+
+*/
+
+const teigiamas = 5;
+const neigiamas = -8;
+const nulis = 0;
+const nulisMinus = -0;
+
+console.log(nulis, nulisMinus);
+
+const desimtainis = 3.141592564;
+console.log(desimtainis);
+
+const begalybe = Infinity;
+const begalybeMinus = -Infinity;
+console.log(begalybe, begalybeMinus);
+
+const nesamone = NaN;
+console.log(nesamone);
+
+const puseZodzio = 'labas' / 2;
+console.log(puseZodzio); /* atsakyme NaN */
+
+
