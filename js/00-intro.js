cde5185c34f53f0a232847e3165dd3da3e371bb2
Eugenijus <augisk@gmail.com>
2021-05-04 18:56:09 +0300

    JS startas

---
+++ js/00-intro.js
@@ -0,0 +1,17 @@
+// single-line comment
+
+/* 
+multiline 
+comment */
+
+/* 
+
+KONCEPTAI
+
+1) kintamieji - isimena informacija 
+2) salygos sakiniai - tarpusavio palyginimas
+3) ciklai - darbas su pasikartojancio formato informacija, pasikartojanciu procesu valdymas
+4) funkcijos - uzdara logika, kuria galima perpanaudoti pagal poreiki
+5) ivykiai - uzsikrove puslapis, paspaude klaviaturos mygtuka, paspaude peles mygtuka, pradejo siusti faija, pabaige siusti faila, issiunte uzklausa ir t.t. 
+6) oop (object orienting programming)  - objektinis programavimas (tai tik skonio reikalas)
+*/
