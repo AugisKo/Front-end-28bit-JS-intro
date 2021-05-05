6e86f2aa89026b0b771d0ced4e651c78156c4471
Eugenijus <augisk@gmail.com>
2021-05-05 18:57:18 +0300

    sekos

---
+++ js/04-array.js
@@ -0,0 +1,20 @@
+/*
+ARRAY
+*/
+
+const marks = [10, 2, 8, 4, 6];
+const size = marks.length;
+
+const firstMark = marks[0];
+const second = marks[1];
+const last = marks[size - 1];
+
+console.log(size);
+console.log (marks);
+console.log(`pirmas:`, firstMark);
+console.log(`antras:`, second);
+console.log(`paskutinis:`, last);
+const dictionary = [`labas`, `rytas`, `sakay`, `tau`];
+
+const paskutinisZodis = dictionary[dictionary.length - 1];
+console.log(paskutinisZodis); 
