485eb1fe139d7dfd7ca31634096ff0097727fdd9
Eugenijus <augisk@gmail.com>
2021-05-06 20:12:51 +0300

    objects

---
+++ js/11-object.js
@@ -0,0 +1,79 @@
+/*
+Objektas yra key-value poru rinkinys (raktazodsi-reiksme
+*/)
+
+const petras = {
+name: 'Petras',
+age: 99
+'likes banana': true
+};
+
+console.log(petras);
+
+const maryte = {
+    name: 'Maryte',
+    age: 87
+    likesBanana: false
+    };
+    
+   /* console.log(petras.age);
+    console.log(maryte.age);
+    console.log(petras.name);
+    console.log(maryte.name);
+*/
+    const gyventojai = [petras, maryte];
+
+    console.log(gyventojai[0]);
+    console.log(gyventojai[1]);
+    console.log(gyventojai[0].name);
+    console.log(gyventojai[1].age);
+
+    const batas = {
+        name: 'Kaliosas 3000',
+        color: 'red',
+        pricing: [
+            {
+                country: 'LT',
+                price: 99
+            },
+            {
+                country: 'LV',
+                price: 89
+            },
+            {
+                country: 'EE',
+                price: 199
+            }
+
+        ]
+    }
+
+    console.log(batas.pricing[0]);
+    console.log(batas.pricing[1]);
+    console.log(batas.pricing[2]);
+
+    const kepure = {
+        name: 'Skrybele',
+        color: 'Balta',
+        pricing: {
+            lt: 99,
+            lv: 89,
+            ee: 199
+        }
+    }
+    
+    console.log(kepure.pricing.lt)
+
+    /* 
+Gyvunas: 
+        vardas, 
+        kailio spalva, 
+        amziu, 
+        megstami patiekalai (pavadinimas, svoris, kaloriju kiekis, prioritetas), 
+        pazymeti medziai ([x,y] koordinates, rusi, auksti)
+
+Gyvunu prekiu parduotuve:
+        pavadinimas,
+        adresas,
+        darbo laikas,
+        asortimentas (pavadinimas, kiekis)
