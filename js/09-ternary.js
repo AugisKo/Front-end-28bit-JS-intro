df413cf67143868742295fa0b5e30421f513949a
Eugenijus <augisk@gmail.com>
2021-05-06 19:26:59 +0300

    Switch

--- js/09-ternary.js
+++ js/09-ternary.js
@@ -10,7 +10,9 @@ const arLyja = false;
 const sketis = arLyja ? 'pasiimk' : 'nereikia';             //pasiimk / nereikia;
 console.log(sketis);
 
-const s1 = 4 < 2 ? 'daugiau' : 'ne daugiau';
+const s1 = 4 < 2 
+            ? 'daugiau' 
+            : 'ne daugiau';
 console.log(s1);
 
 
@@ -31,3 +33,29 @@ if (4 < 2) {
 } 
 
 console.log(s3);
+
+console.log ('---------------------------');
+/*
+Nestintas Ternary
+*/
+const color = 'yellow';
+
+const x1 = color === 'blue' 
+                ? 'Melyna' 
+                : color === 'red' 
+                ? 'raudona' 
+                : 'nezinoma spalva';
+
+console.log(x1);
+
+const color = 'blue';
+const light = true;
+const x2 = color === 'blue' 
+                ? light 
+                    ? 'Sviesiai melyna' 
+                    : 'Tamsiai melyna' 
+                : color === 'red' 
+                ? 'raudona' 
+                : 'nezinoma spalva';
+
+console.log(x2);
